3ce3792c0abd551c6ece220a791cae7400033828
Jennifer Hu <jhu890@gmail.com>
2024-06-07 10:07:30 -0400

    Update Exp2 and Exp3 stats

--- analysis/r_scripts/exp2_analyses.R
+++ analysis/r_scripts/exp2_analyses.R
@@ -11,7 +11,9 @@ output_dir <- "analysis/r_output"
 df <- read.csv("data/exp2_model_surprisal/surprisal_ngram_data.csv")
 df$condition <- factor(
   df$condition, 
-  levels=c("probable", "improbable", "impossible_physics", "impossible_magic", "inconceivable_semantic", "inconceivable_syntactic")
+  levels=c("probable", "improbable", 
+           "impossible_physics", "impossible_magic", 
+           "inconceivable_semantic", "inconceivable_syntactic")
 )
 
 # Iterate over models.
@@ -19,13 +21,6 @@ for (model_name in unique(df$model)) {
   # Get data corresponding to this model.
   safe_model_name = sapply(strsplit(model_name, "/"), tail, 1)
   data = df %>% filter(model==model_name)
-
-  # ANOVA: check if mean surprisals are different across conditions.
-  m.aov <- data %>% anova_test(continuation_sum_surprisal ~ condition)
-  write.csv(m.aov, sprintf("%s/%s_means_anova.csv", output_dir, safe_model_name))
-  # Post-hoc pairwise comparisons.
-  pwc <- data %>% tukey_hsd(continuation_sum_surprisal ~ condition)
-  write.csv(pwc, sprintf("%s/%s_means_tukey.csv", output_dir, safe_model_name))
   
   # Prepare data to fit LMER models.
   # First, remove rows where log count is -inf.
@@ -33,37 +28,21 @@ for (model_name in unique(df$model)) {
   # Next, backward difference coding of condition: 
   # https://stats.oarc.ucla.edu/r/library/r-library-contrast-coding-systems-for-categorical-variables/#backward
   backward.diff = matrix(c(
-    -5/6, 1/6, 1/6, 1/6, 1/6, 1/6, 
-    -4/6, -4/6, 2/6, 2/6, 2/6, 2/6,
-    -3/6, -3/6, -3/6, 3/6, 3/6, 3/6,
-    -2/6, -2/6, -2/6, -2/6, 4/6, 4/6,
-    -1/6, -1/6, -1/6, -1/6, -1/6, 5/6
+    -5/6, 1/6, 1/6, 1/6, 1/6, 1/6, # 1: improbable vs probable
+    -4/6, -4/6, 2/6, 2/6, 2/6, 2/6, # 2: impossible_physics vs improbable
+    -3/6, -3/6, -3/6, 3/6, 3/6, 3/6, # 3: impossible_magic vs impossible_physics
+    -2/6, -2/6, -2/6, -2/6, 4/6, 4/6, # 4: inconceivable_semantic vs impossible_magic
+    -1/6, -1/6, -1/6, -1/6, -1/6, 5/6 # 5: inconceivable_syntactic vs inconceivable_semantic
   ), ncol = 5)
   contrasts(data$condition) = backward.diff
-
-  # Fit LMER model with *no* ngram predictor.
-  m_no_ngram <- lmer(
-    continuation_sum_surprisal ~ condition + (1 | item_id),
-    data=data
-  )
-  sink(sprintf("%s/%s_lmer_no_ngram.csv", output_dir, safe_model_name))
-  print(summary(m_no_ngram))
-  sink()
   
-  # Fit LMER model *with* ngram predictor.
-  m_with_ngram <- lmer(
+  # Fit LMER model with ngram predictor.
+  m <- lmer(
     continuation_sum_surprisal ~ condition + log_joint_count + (1 | item_id),
     data=data
   )
-  sink(sprintf("%s/%s_lmer_with_ngram.csv", output_dir, safe_model_name))
-  print(summary(m_with_ngram))
+  sink(sprintf("%s/exp2_%s_surprisal_vs_condition.txt", output_dir, safe_model_name))
+  print(summary(m))
   sink()
-  
-  # Compare model 1 to model 2 using ANOVA.
-  comparison <- anova(m_no_ngram, m_with_ngram)
-  write.csv(
-    comparison, 
-    sprintf("%s/%s_lmer_comparison.csv", output_dir, safe_model_name)
-  )
 }
 
--- analysis/r_scripts/exp3_analyses.R
+++ analysis/r_scripts/exp3_analyses.R
@@ -7,6 +7,24 @@ library("lmerTest")
 df <- read.csv("data/exp3_human_rt/critical_clean.csv")
 output_dir <- "analysis/r_output"
 
+# Test for difference in mean RT between condition pairs.
+t.test(
+  filter(df, condition=="inconceivable_semantic")$rt_normalized,
+  filter(df, condition=="improbable")$rt_normalized,
+  alternative="less"
+)
+t.test(
+  filter(df, condition=="inconceivable_semantic")$rt_normalized,
+  filter(df, condition=="impossible_physics")$rt_normalized,
+  alternative="less"
+)
+t.test(
+  filter(df, condition=="inconceivable_semantic")$rt_normalized,
+  filter(df, condition=="impossible_magic")$rt_normalized,
+  alternative="less"
+)
+
+
 for (model_name in c("gpt2", "Llama-2-7b-hf", "Mistral-7B-v0.1")) {
   # Read model surprisal data.
   model_df <- read.csv(
@@ -16,16 +34,34 @@ for (model_name in c("gpt2", "Llama-2-7b-hf", "Mistral-7B-v0.1")) {
   # Combine human binary judgment data with model surprisal data.
   data <- merge(df, model_df, by=c("item_id", "condition"))
   
-  # Fit model.
+  # Fit judgment vs surprisal model.
   m <- glmer(
     response ~ continuation_sum_surprisal + (1 | subject_id) + (1 | item_id), 
     family=binomial(link="logit"),
     data=data
   )
-  
   # Write summary to file.
-  sink(sprintf("%s/%s_glmer_judgment_vs_surprisal.csv", output_dir, model_name))
+  sink(sprintf("%s/exp3_%s_judgment_vs_surprisal.txt", output_dir, model_name))
   print(summary(m))
+  sink()
+  
+  # Fit RT vs surprisal models.
+  m.1 <- lm(
+    rt_normalized ~ continuation_sum_surprisal,
+    data=data
+  )
+  print(summary(m.1))
+  m.2 <- lm(
+    rt_normalized ~ poly(continuation_sum_surprisal, 2),
+    data=data
+  )
+  # Write summary to file.
+  sink(sprintf("%s/exp3_%s_rt_vs_surprisal.txt", output_dir, model_name))
+  print(summary(m.1))
+  print(cor.test(data$rt_normalized, data$continuation_sum_surprisal))
+  print(summary(m.2))
+  print(sprintf("BIC for first-order model: %f", BIC(m.1)))
+  print(sprintf("BIC for second-order model: %f", BIC(m.2)))
   sink()
 }
 
